e1b2b5ff4ee3fe604f01e4e180f72589b4509464
kaiba55 <wojtekksiazekk@gmail.com>
2017-01-27 11:13:10 +0100

    Przygotowanie klasy waluta i lista walut

---
+++ WpfApplication1/Currency.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    class Currency
+    {
+        private string name;
+        private string symbol;
+        private double purchaseValue;
+        private double purchaseChange;
+        private double purchaseChangePercent;
+        private double saleValue;
+        private double saleChange;
+        private double saleChangePercent;
+
+        public Currency(string name, string symbol, List<double>arguments)
+        {
+            this.name = name;
+            this.symbol = symbol;
+            purchaseValue = arguments[0];
+            purchaseChange= arguments[1];
+            purchaseChangePercent= arguments[2];
+            saleValue = arguments[3];
+            saleChange = arguments[4];
+            saleChangePercent = arguments[5];
+        }
+    }
+}
---
+++ WpfApplication1/ListOfCurrency.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    class ListOfCurrency
+    {
+        List<Currency> listCurrenct;
+        public ListOfCurrency()
+        {
+            listCurrenct = new List<Currency>();
+        }
+
+        void add(Currency currency)
+        {
+            listCurrenct.Add(currency);
+        }
+    }
+}
